fb5a751d528b139c387368ae410bb2ec6941986e
Dani Alcala <daniamcode@gmail.com>
2022-03-17 22:57:50 +0100

    reorganize madlibs file

--- madlibs.py
+++ madlibs.py
@@ -1,19 +1,7 @@
-print("My first program in Python")
-color = 'red'
-number = 3
-print("Color is " + str(color) + " and number is " + str(number))
-my_phrase = "This is a phrase to change into numbers in certain circumstances"
+# We want to write a paragraph with blank spaces that will be variables
 
-def translate(phrase):
-    try:
-        translation = ""
-        for letter in phrase:
-            if letter in "abc":
-                translation = translation + str(8)
-            else:
-                translation = translation + letter
-        return translation
-    except Exception as error:
-        print(error)
-
-print(translate(my_phrase))
+#3 possible ways
+color = "red"
+print("My favourite color is " + color)
+print("My favourite color is {}".format(color))
+print(f"My favourite color is {color}")
